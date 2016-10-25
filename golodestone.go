71054bf7e883389c2c1c3dca856f1a155cd47545
Aleksandr Tihomirov <hunter@zeta.pm>
2016-10-25 22:45:52 +0300

    Adds tests for weather and TimeFrom func.

--- golodestone.go
+++ golodestone.go
@@ -10,7 +10,12 @@ const eorzeaMultiplier float64 = 3600.0 / 175
 
 // Time Gets the time from Eorzea
 func Time() time.Time {
-	nowTicks := time.Now().UTC().Unix()
-	eorzeaTicks := xmath.Round(eorzeaMultiplier * float64(nowTicks))
+	return TimeFrom(time.Now())
+}
+
+// TimeFrom Gets the time from Eorzea for `from` time.Time
+func TimeFrom(from time.Time) time.Time {
+	fromTicks := from.UTC().Unix()
+	eorzeaTicks := xmath.Round(eorzeaMultiplier * float64(fromTicks))
 	return time.Unix(eorzeaTicks, 0).UTC()
 }
